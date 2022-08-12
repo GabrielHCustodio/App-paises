e10538969ea109ec9218d7b9dda8b0b79b5653fd
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-08-12 20:01:32 -0300

    This is my first commit from my country app project.

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Paises do mundo</h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    created() {
+        fetch('https://restcountries.com/v3.1/all')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+    }
+}
+</script>
